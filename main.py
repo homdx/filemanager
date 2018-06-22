3dd2571eae8577fecc4f94b5a8b83600ec1e2bb5
rotex5 <dantejake48@gmail.com>
2018-06-22 17:47:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+
 from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 import os
-from os.path import basename, isfile
 import shutil
 import psutil
 import zlib
 
 
 
+class PopUps(BoxLayout):
+    cancel = ObjectProperty(None)
 
 
 
 class MyWidget(BoxLayout):
 
-    path_way = ObjectProperty(None)
+
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def finish_popup(self, operation_type):
+        content = PopUps(cancel=self.dismiss_popup)
+        self._popup = Popup(title=operation_type, content=content, size_hint=(0.3, 0.3))
+        self._popup.open()
 
 
 
@@ -52,21 +63,36 @@ class MyWidget(BoxLayout):
 
 
 
-    def paste_file(self, path, filename):
+    def paste_file(self, path, filename, symlinks=False, ignore=None):
         source = self.copy_file(path, filename)
         destination = self.get_selected_directory(path)
-        shutil.copy2(source, destination)
-        print("Done Copying!!")
+
+        if os.path.isdir(source):
+            shutil.copytree(source, destination, symlinks, ignore)
+            self.finish_popup("Copy Operation")
+            print("Folder Copied!!")
+        else:
+            shutil.copy2(source, destination)
+            self.finish_popup("Copy Operation")
+            print("File Copied!!")
 
 
 
-        pass
+
 
     def delete_file(self, path, filename):                               #deleting file
         sfile = self.get_selected_file(path, filename)
         print(sfile)
-        os.remove(sfile)                                                 #it finally works #delete the matching  item
-        print('Deletion Complete!!!!')
+        if os.path.isdir(sfile):
+            shutil.rmtree(sfile)                                                 #it finally works #delete the matching  item
+            self.finish_popup("Deletion Operation")
+
+            print('Folder Deleted!!')
+        else:
+            os.remove(sfile)
+            self.finish_popup("Deletion Operation")
+
+            print("File Deleted!!")
 
     def compress_file(self, path, filename):                             #Fuction for file compression using zlib module
         cfile = self.get_selected_file(path, filename)                   #call the get_selected_file methode
@@ -75,6 +101,7 @@ class MyWidget(BoxLayout):
         comp_File = open(f_Name + '(compressed)' + f_Extension, 'wb')
         comp_File.write(zlib.compress(normal_File,9))
         comp_File.close()
+        self.finish_popup("Compression Operation")
         print("Compression Complete!!!")
 
     def decompress_file(self, path, filename):                            #Fuction for file Decompression using zlib module
@@ -85,8 +112,8 @@ class MyWidget(BoxLayout):
         decomp_File = open(f_Name + f_Extension, 'wb')
         decomp_File.write(zlib.decompress(comp_File))
         decomp_File.close()
+        self.finish_popup("Decompression Operation")
         print("decompression Complete!!!")
-
 
 
 class MyApp(App):
